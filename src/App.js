b66c88c2191dd8d47c3674dceb6e63e1d4a7ca02
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2019-05-04 17:59:44 +0200

    added dashboard component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar/>
+        <Dashboard/>
       </div>
     </BrowserRouter>
   );
---
+++ src/components/dashboard/Notifications.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notifications = () => {
+    return (
+        <div>
+            <p>NOTIFICATIONS</p>
+        </div>
+    );
+};
+
+export default Notifications;
