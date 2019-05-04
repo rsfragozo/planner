f175f525b843d8b2d030d49ac0b5b8e5cbe8251e
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2019-05-04 18:14:25 +0200

    added ProjectSummary

--- src/components/project/ProjectList.js
+++ src/components/project/ProjectList.js
@@ -1,29 +1,12 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content gery-text text-darken-3">
-                    <span className="card-title">Project 1</span>
-                    <p>Posted by me</p>
-                    <p className="grey-text">3-10-2011 11:11</p>
-                </div>
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content gery-text text-darken-3">
-                    <span className="card-title">Project 1</span>
-                    <p>Posted by me</p>
-                    <p className="grey-text">3-10-2011 11:11</p>
-                </div>
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content gery-text text-darken-3">
-                    <span className="card-title">Project 1</span>
-                    <p>Posted by me</p>
-                    <p className="grey-text">3-10-2011 11:11</p>
-                </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     );
 };
---
+++ src/components/project/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content gery-text text-darken-3">
+                <span className="card-title">Project 1</span>
+                <p>Posted by me</p>
+                <p className="grey-text">3-10-2011 11:11</p>
+            </div>
+        </div>
+    );
+};
+
+export default ProjectSummary;
