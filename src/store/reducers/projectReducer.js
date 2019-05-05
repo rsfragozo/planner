edc181c5d7012c58ebedd13d51d666e6b83cde81
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2019-05-05 14:49:13 +0200

    added redux, created stores and added dummy data to dashboard

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+    projects: [
+        { id: 1, title: 'title 1', content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore amet error ullam earum praesentium quasi voluptate corrupti ratione, ex quibusdam eligendi rerum distinctio facilis veritatis pariatur alias eaque accusamus fugit?' },
+        { id: 2, title: 'title 2', content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore amet error ullam earum praesentium quasi voluptate corrupti ratione, ex quibusdam eligendi rerum distinctio facilis veritatis pariatur alias eaque accusamus fugit?' },
+        { id: 3, title: 'title 3', content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore amet error ullam earum praesentium quasi voluptate corrupti ratione, ex quibusdam eligendi rerum distinctio facilis veritatis pariatur alias eaque accusamus fugit?' }
+    ]
+};
+
+const projectReducer = (state = initState, action) => {
+    return state;
+}
+
+export default projectReducer;
