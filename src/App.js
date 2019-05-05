9050732ef1a0f9badc71f58f5d5ca47484edf24d
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2019-05-05 10:44:11 +0200

    added SignIn and SignUp components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/project/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Navbar/>
+        <Navbar />
         <Switch>
-          <Route exact path='/' component={Dashboard}/>
+          <Route exact path='/' component={Dashboard} />
           <Route path='/project/:id' component={ProjectDetails} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -5,8 +5,8 @@ import { NavLink } from 'react-router-dom';
 const SignedOutLinks = () => {
     return (
         <ul className="right">
-            <li><NavLink to='/'>Signup</NavLink></li>
-            <li><NavLink to='/'>Login</NavLink></li>
+            <li><NavLink to='/signup'>Sign up</NavLink></li>
+            <li><NavLink to='/signin'>Log in</NavLink></li>
         </ul>
     );
 }
