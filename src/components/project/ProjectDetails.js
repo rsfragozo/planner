2505a89892db871be317ee23f3cd57655bd08e77
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2019-05-09 22:35:46 +0200

    added moment for daet format

--- src/components/project/ProjectDetails.js
+++ src/components/project/ProjectDetails.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
 import { Redirect } from 'react-router-dom';
+import moment from 'moment';
 
 const ProjectDetails = (props) => {
     const { project, auth } = props;
@@ -20,7 +21,7 @@ const ProjectDetails = (props) => {
                     </div>
                     <div className="card-action great lighten-4 grey-text">
                         <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
-                        <p>SEPT 222</p>
+                        <p>{ moment(project.createDate.toDate()).calendar() }</p>
                     </div>
                 </div>
             </div>
--- src/components/project/ProjectSummary.js
+++ src/components/project/ProjectSummary.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import moment from 'moment';
 
 const ProjectSummary = ({ project }) => {
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-content gery-text text-darken-3">
                 <span className="card-title">{ project.title }</span>
-                <p>Posted by me</p>
-                <p className="grey-text">3-10-2011 11:11</p>
+                <p>Posted by { project.authorFirstName } { project.authorLastName }</p>
+                <p className="grey-text">{ moment(project.createDate.toDate()).calendar() }</p>
             </div>
         </div>
     );
